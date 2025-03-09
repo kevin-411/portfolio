de049c533e4d9bf492e501095ea4a9fd13ca7964
kodhiambo <k.odhiambo@students.jkuat.ac.ke>
2025-03-09 10:48:57 +0300

    initial commit

---
+++ src/components/Projects.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    name:'Projects',
+    methods:{
+        goToProjects(){this.$router.push({ name: 'Projects' })}
+    }
+}
+</script>
+
+<style scoped>
+/* Shine */
+.hover14 figure {
+	position: relative;
+}
+.hover14 figure::before {
+	position: absolute;
+	top: 0;
+	left: -75%;
+	z-index: 2;
+	display: block;
+	content: '';
+	width: 50%;
+	height: 100%;
+	background: -webkit-linear-gradient(left, rgba(255,255,255,0) 0%, rgba(255,255,255,.3) 100%);
+	background: linear-gradient(to right, rgba(255,255,255,0) 0%, rgba(255,255,255,.3) 100%);
+	-webkit-transform: skewX(-25deg);
+	transform: skewX(-25deg);
+}
+.hover14 figure:hover::before {
+	-webkit-animation: shine 1.5s;
+	animation: shine 1.5s;
+}
+@-webkit-keyframes shine {
+	100% {
+		left: 125%;
+	}
+}
+@keyframes shine {
+	100% {
+		left: 125%;
+	}
+}
+</style>
---
+++ src/components/links.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex space-x-10 2xl:text-4xl text-3xl slit-in-diagonal-1">
+    <div>
+      <a href="https://twitter.com/odhiambokev11" target="_blank"
+        ><i class="fab fa-twitter-square text-gray-800 dark:text-blueGray-300 dark:hover:text-turquoise hover:text-turquoise"></i
+      ></a>
+    </div>
+    <div>
+      <a href="https://www.linkedin.com/in/kevin-obuya-685450161" target="_blank"
+        ><i class="fab fa-linkedin text-gray-800 dark:text-blueGray-300 dark:hover:text-turquoise hover:text-turquoise"></i
+      ></a>
+    </div>
+    <div>
+      <a href="https://github.com/kevin-411" target="_blank"
+        ><i class="fab fa-github-square text-gray-800 dark:text-blueGray-300 dark:hover:text-turquoise hover:text-turquoise"></i
+      ></a>
+    </div>
+    <!-- <div>
+      <a href="https://telegram.me/ahmedalmory" target="_blank">
+        <i class="fab fa-telegram-plane text-gray-800 dark:text-blueGray-300 dark:hover:text-turquoise hover:text-turquoise"></i></a>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+    name:'VLinks'
+};
+</script>
